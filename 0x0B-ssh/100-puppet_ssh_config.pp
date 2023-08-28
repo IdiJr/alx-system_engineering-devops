5dd46d10741d278f4975c9da10a4bece44a1f82d
IdiJr <idialijnr@gmail.com>
2023-08-28 13:07:26 +0000

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,17 @@
 #set up your client SSH configuration file so that you can connect to a server without typing a password.
-exec {'ssh':
-  command => 'echo "IdentityFile ~/.ssh/school\nPasswordAuthentication no" >> /etc/ssh/ssh_config',
-  path    => '/usr/bin',
-  }
+
+include stdlib
+
+file_line { 'password authenticticate off':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'identity file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}
