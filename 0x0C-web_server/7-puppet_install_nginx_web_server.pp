50ef88a7fb716428d8aba6242ba0a7232cf484e7
IdiJr <idialijnr@gmail.com>
2023-08-30 05:35:23 +0000

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,7 +1,6 @@
 # Configures an Nginx server using Puppet instead of Bash.
 
-package {
-    'nginx':
+package { 'nginx':
     ensure => installed,
 }
 
@@ -9,12 +8,13 @@ file {'/var/www/html/index.html':
     content => 'Hello World!',
 }
 
-file_line {'configure redirection':
-    path  =>  '/etc/nginx/sites-available/default',
+file_line { 'redirection configuration':
+    path  =>  '/etc/nginx/sites-enabled/default',
     after =>  'server_name _;',
-    line  =>  "\n\tlocation /redirect_me {\n\t\treturn 301 https://youtu.be/nuNvRFOXn3w?si=JvnxyjMZY0E4ifC4 permanent;\n\t}\n",
+    line  =>  'rewrite ^/redirect_me https://youtu.be/nuNvRFOXn3w?si=JvnxyjMZY0E4ifC4 permanent;',
 }
 
-service {'nginx':
-    ensure => running,
+service { 'nginx':
+  ensure  => running,
+  require => Package[ 'nginx' ],
 }
