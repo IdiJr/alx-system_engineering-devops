4efec3d6516aa48f531584695102858d6adb5ef6
IdiJr <idialijnr@gmail.com>
2023-09-26 06:35:20 +0000

    Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+This script retrieves and displays TODO list progress for a
+given employee ID using a REST API and exports data in
+the JSON format.
+"""
+
+import requests
+from sys import argv
+import json
+
+if __name__ == "__main__":
+    if len(argv) != 1:
+        print("Usage: {} <employee_id>".format(argv[0]))
+        exit(1)
+
+    base_url = "https://jsonplaceholder.typicode.com"
+    users_url = f"{base_url}/users"
+    users_response = requests.get(users_url)
+
+    if users_response.status_code != 200:
+        print("Failed to fetch user data")
+        exit(1)
+
+    users_data = users_response.json()
+    todo_dict = {}
+
+    for user in users_data:
+        user_id = user["id"]
+        username = user["username"]
+        todos_url = f"{base_url}/todos?userId={user_id}"
+        todos_response = requests.get(todos_url)
+
+        if todos_response.status_code != 200:
+            print(f"Failed to fetch tasks for user {username}")
+            continue
+
+        todos_data = todos_response.json()
+        user_tasks = []
+
+        for task in todos_data:
+            user_task = {
+                "username": username,
+                "task": task["title"],
+                "completed": task["completed"]
+            }
+            user_tasks.append(user_task)
+
+        todo_dict[str(user_id)] = user_tasks
+
+    with open("todo_all_employees.json", "w") as json_file:
+        json.dump(todo_dict, json_file, separators=(',', ':'))
+
+    print("Data exported to 'todo_all_employees.json'")
