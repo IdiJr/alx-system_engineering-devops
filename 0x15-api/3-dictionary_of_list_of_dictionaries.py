63975d6f04ced518999322dac6643009da4fe06f
IdiJr <idialijnr@gmail.com>
2023-09-26 06:40:50 +0000

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -50,5 +50,3 @@ if __name__ == "__main__":
 
     with open("todo_all_employees.json", "w") as json_file:
         json.dump(todo_dict, json_file, separators=(',', ':'))
-
-    print("Data exported to 'todo_all_employees.json'")
